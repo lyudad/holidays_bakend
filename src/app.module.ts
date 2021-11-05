833694ce3ab4e0026527fc6d12e89e8bf8abfb55
alex-kurkov <alexkourkov@yandex.ru>
2021-11-05 22:28:23 +0500

    add DB & ORM presets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const {
+  DB_PORT = 3306,
+  DB_HOST = 'localhost',
+  DB_USERNAME = 'root',
+  DB_PASSWORD = 'root',
+  DATABASE = 'vacation',
+  NODE_ENV = 'development',
+} = process.env;
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: DB_HOST,
+      port: Number(DB_PORT),
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      database: DATABASE,
+      entities: [],
+      synchronize: NODE_ENV === 'production' ? false : true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import 'reflect-metadata';
+import dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
+const { PORT = 3000 } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
