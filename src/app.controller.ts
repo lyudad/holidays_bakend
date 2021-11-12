bbe0f75f3abd558b161b17809815dfe58aacd413
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-11-12 18:38:13 +0000

    delete some old info

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,7 @@
-// import { Controller, Get, Post } from '@nestjs/common';
+// import { Controller, Get } from '@nestjs/common';
 // import { AppService } from './app.service';
 
 // @Controller()
 // export class AppController {
 //   constructor(private readonly appService: AppService) {}
-
-//   @Get('/users')
-//   getListUsers(): string {
-//     return this.appService.getListUsers();
-//   }
-
-//   @Post('/users')
-//   postListUsers(): string {
-//     return this.appService.postListUsers();
-//   }
 // }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,3 @@
-// import { Injectable } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -7,13 +6,3 @@ export class AppService {
     return 'Hello World!';
   }
 }
-// @Injectable()
-// export class AppService {
-//   getListUsers(): string {
-//     return 'All users are here';
-//   }
-
-//   postListUsers(): string {
-//     return 'All POST users are here';
-//   }
-// }
